a53582d97e7249cc2062bfd1e06b46808a907f0a
23stas23 <2502567@uad.ac.uk>
2026-04-11 14:58:33 +0100

    Added level loader, also i add that loaded tilmap but no conect to head level

--- Coursework/CMP105App/LevelLoader.cpp
+++ Coursework/CMP105App/LevelLoader.cpp
@@ -1,14 +1,13 @@
 #include "LevelLoader.h"
 
-LevelLoader::LevelLoader() {
+LevelLoader::LevelLoader(sf::RenderWindow& window, Input& input, GameState& gameState, AudioManager& audio) {
+	
+
+}
+
+void LevelLoader::MapSetup(std::string tileMapData, sf::Vector2u mapDimensions, int tile_size, int num_columns, int num_rows, int sheet_spacing, std::string Texture) {
 	GameObject tile;
 	std::vector<GameObject> tileSet;
-
-	int num_columns = 20;
-	int num_rows = 9;
-	int tile_size = 18;      // Visual size of the tile
-	int sheet_spacing = 1;   // Gap between tiles
-
 
 	// Set GameObject size (Scaling up 4x for visibility)
 	// 4 * 18 = 3 * 24 = 72 (dino size is 24).
@@ -34,8 +33,17 @@ LevelLoader::LevelLoader() {
 	tile.setCollider(false);
 	tileSet.push_back(tile);
 
-	sf::Vector2u mapDimensions{ 40, 8 };
+	//Get tileMap//
+	int a;
 	std::vector<int> tileMap;
+	std::ifstream data(tileMapData);
+	if (!data.is_open()) std::cerr << "Failed to open data \n";
+	int i = 0;
+	while (data >> a) {
+		tileMap.push_back(a);
+	}
+	data.close();
+	//__________//
 
 	m_tilemap.loadTexture("gfx/tilemap.png");
 	m_tilemap.setTileSet(tileSet);
@@ -44,35 +52,4 @@ LevelLoader::LevelLoader() {
 	m_tilemap.buildLevel();
 
 	tileSet.clear();
-
-	// setup background
-	tile_size = 24;
-	num_columns = 8;
-	num_rows = 3;
-	// 24 * 9 = 216, a multiple of 72, the LCM of the player and tile size.
-	tile.setSize(sf::Vector2f(tile_size * 9, tile_size * 9));
-
-	for (int i = 0; i < num_columns * num_rows; i++)
-	{
-		int row = i / num_columns;
-		int col = i % num_columns;
-
-		tile.setTextureRect({
-			{(tile_size + sheet_spacing) * col, (tile_size + sheet_spacing) * row},
-			{tile_size, tile_size} });
-		tile.setCollider(false);		// don't collide with background
-		tileSet.push_back(tile);
-	}
-
-	mapDimensions = { 14,3 };
-	tileMap = {
-		6,6,6,6,6,6,6,6,6,6,6,6,6,6,
-		14,14,14,14,14,14,14,14,14,14,14,14,14,14,
-		22,22,22,22,22,22,22,22,22,22,22,22,22,22,22,22
-	};
-	m_bgtilemap.loadTexture("gfx/tilemap-backgrounds.png");
-	m_bgtilemap.setTileSet(tileSet);
-	m_bgtilemap.setTileMap(tileMap, mapDimensions);
-	m_bgtilemap.setPosition({ 0, 0 });
-	m_bgtilemap.buildLevel();
 }
